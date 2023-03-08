f4af5360a2efb8f8a38f2e375cce0ff11e404cca
Dani Gevorkian <danigevorkian@gmail.com>
2023-03-08 16:14:35 +0100

    Sigue siendo configuración para empezar el proyecto

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -2,7 +2,6 @@ export const HeroesApp = () => {
   return (
     <>
         <h1>HeroesApp</h1>
-        <button className="btn btn-primary">Boton</button>
     </>
   )
 }
---
+++ src/assets/README.md
@@ -0,0 +1,3 @@
+## ¿QUÉ SON LOS ASSETS?
+
+Son ficheros estáticos que no cambian, como documentos, imágenes, archivos de audio, vídeo, etc
